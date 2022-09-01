a34b550029bac8da1e59e6ab3d2ea75de1b5bc3a
vPankaja <vpankaja42@gmail.com>
2022-09-01 19:44:18 +0530

    backend initial commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js'
+import cors from 'cors'
+
+dotenv.config()
+
+//connect database
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+//create port
+const PORT = process.env.PORT || 9000
+
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} port ${PORT}`
+  )
+)
