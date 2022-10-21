1c4e4a0268f8639253819aa3f53c74ca870a15dd
nadeesh9 <nadeeshedi@gmail.com>
2022-10-21 19:20:04 +0530

    fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
-//Connection file to mongo db
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
-    })
-    //database connected alert
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.bold);
-    process.exit();
-  }
-};
+      //database connection
+      try {
+            const conn = await mongoose.connect(process.env.MONGODB_URL, {
+            })
+            //database connected alert
+            console.log(`MongoDB Connected: ${conn.connection.host}`)
+      } catch (error) {
+            console.error(`Error: ${error.message}`) //database not connected message
+            process.exit(1)
+      }
+}
 
-export default connectDB;
+export default connectDB  //exporting database
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-// import colors from 'colors';
-import path from 'path';
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js'
+import cors from 'cors'
+import path from "path"
+
+// Routes
+import boatRoutes from './routes/boat.routes.js'
+import InvManagerRoutes from "./routes/inv.manage.routes.js"
+import OrderRoutes from './routes/OrderRoutes.js'
+import BillRoutes from './routes/BillRoutes.js'
+import ProductRoutes from './routes/ProductRoutes.js'
 
 import noteRoutes from './routes/noteRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -14,34 +22,54 @@ connectDB();
 
 const app = express(); // main thing
 
+app.use(cors())
 app.use(express.json()); // to accept json data
 
+app.use("/api/invManager", InvManagerRoutes);
+app.use('/api/boats', boatRoutes);
+app.use('/order',OrderRoutes);
+app.use('/bill',BillRoutes);
+app.use('/product',ProductRoutes);
 app.use('/api/notes', noteRoutes);
 app.use('/api/users', userRoutes);
 
-// --------------------------deployment------------------------------
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running..');
-  });
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
-// --------------------------deployment------------------------------
 
-// Error Handling middlewares
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
+//create port
+const PORT = process.env.PORT || 9000
 
 app.listen(
   PORT,
-  console.log(`Server running in dev mode on port ${PORT}..`.yellow)
-);
+  console.log(
+    `server running in ${process.env.NODE_ENV} port ${PORT}`
+  )
+)
+
+// --------------------------deployment------------------------------
+// const __dirname = path.resolve();
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   );
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running..');
+//   });
+// }
+// // --------------------------deployment------------------------------
+
+// // Error Handling middlewares
+// app.use(notFound);
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(
+//   PORT,
+//   console.log(`Server running in dev mode on port ${PORT}..`.yellow)
+// );
